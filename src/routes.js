b606c24d102fbc7ccc6545a90faf238facece299
andrehenrique1986 <andrehenriquelima1986@gmail.com>
2025-01-20 09:24:07 -0300

    Criando as Rotas, Cabeçalho e Rodapé

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./pages/Inicio";
+
+
+const AppRoutes = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Inicio/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+} 
+
+export default AppRoutes;
